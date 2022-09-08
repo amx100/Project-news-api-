07638fc1a65d1702a349db98944f14aaa0e34aa5
Ahned <amx281@gmail.com>
2022-09-08 03:31:09 +0200

    landingPage

--- my-app/src/Functional/Router/Landing Page/landingPage.jsx
+++ my-app/src/Functional/Router/Landing Page/landingPage.jsx
@@ -1,32 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import styles from "./Card.module.css";
 
 function App() {
   const [news, setNews] = useState([]);
+  const [page, setPage] = useState(1);
+  const [word, setWord] = useState();
 
   const fetchNews = () => {
     axios
       .get(
-        "https://newsapi.org/v2/everything?q=tesla&from=2022-08-01&sortBy=publishedAt&apiKey=84a88c116e674bfd94b0bf91878ab59a"
+        `https://newsapi.org/v2/everything?q=tesla&&pageSize=20&page=${page}&sortBy=publishedAt&apiKey=29a06d6161374b318b0ca61b0d58b03f`
       )
       .then((response) => {
         console.log(response);
-        setNews(response.data.articles);
+        setNews([...news, ...response.data.articles]);
       });
   };
+  useEffect(() => {
+    fetchNews();
+  }, [page]);
+
   return (
     <>
-      <div className="container my-3">
-        <div className="row">
-          <div className="col-4">
-            <button className={styles.button} onClick={fetchNews}>
-              Fetch News
-            </button>
-          </div>
-        </div>
-      </div>
-
       <div className="container">
         <div className={styles.row}>
           {news.map((value, index) => {
@@ -41,7 +37,7 @@ function App() {
                   <div className="card-body">
                     <h5 className="card-title">{value.title?.slice(0, 70)}</h5>
                     <p className={styles.cardText}>
-                      {value.description?.slice(0, 100)}
+                      {value.description?.slice(0, 80)}
                     </p>
                     <a href={value.url} className={styles.button}>
                       Read More...
@@ -51,6 +47,20 @@ function App() {
               </div>
             );
           })}
+        </div>
+      </div>
+      <div className="container my-3">
+        <div className="row">
+          <div className="col-4">
+            <button
+              className={styles.button}
+              onClick={() => {
+                setPage(page + 1);
+              }}
+            >
+              Fetch News
+            </button>
+          </div>
         </div>
       </div>
     </>

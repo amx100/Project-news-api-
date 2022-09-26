8bdbf5261c0755f1025de099de52b3c927845703
Ahmed <amx281@gmail.com>
2022-09-26 02:01:21 +0200

    Finished project

--- my-app/src/Functional/Router/Landing Page/landingPage.jsx
+++ my-app/src/Functional/Router/Landing Page/landingPage.jsx
@@ -5,14 +5,37 @@ import apiNews from "../Api/api";
 function App() {
   const [news, setNews] = useState([]);
   const [page, setPage] = useState(1);
-  const [search, setSearch] = useState("tesla");
   const [value, setValue] = useState("");
+
+  const [search, setSearch] = useState("daily");
+
   const [category, setCategory] = useState("general");
   const [newcategory, SetnewCategory] = useState("popularity");
   const [drop, setDrop] = useState(false);
 
+  const fetchNews = async () => {
+    try {
+      let result = "";
+      if (search !== "") {
+        result = await apiNews.get(
+          `everything?&q=${search}&pageSize=20&page=${page}&apiKey=84a88c116e674bfd94b0bf91878ab59a`
+        );
+      } else {
+        result = await apiNews.get(
+          `top-headlines?country=us&category=${category}&pageSize=20&page=${page}&apiKey=84a88c116e674bfd94b0bf91878ab59a`
+        );
+      }
+
+      const data = result.data;
+      console.log(result.data.articles);
+      setNews([...news, ...data.articles]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   // const fetchNews = () => {
-  //   axios
+  //   apiNews
   //     .get(
   //       `https://newsapi.org/v2/everything?sortBy=${category}&q=${search}&pageSize=20&page=${page}&apiKey=29a06d6161374b318b0ca61b0d58b03f`
   //     )
@@ -22,27 +45,6 @@ function App() {
   //     });
   // };
 
-  const fetchNews = async () => {
-    try {
-      let result;
-      if (search !== "") {
-        result = await apiNews.get(
-          `everything?sortBy=${newcategory}&q=${search}&pageSize=20&page=${page}&apiKey=9770eaa81d774e98881c1153166cdbd2`
-        );
-      } else {
-        result = await apiNews.get(
-          `top-headlines?country=us&category=${category}&page=${page}&apiKey=9770eaa81d774e98881c1153166cdbd2`
-        );
-      }
-
-      const data = result.data.articles;
-      setNews(data);
-      console.log(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  /*  */
   useEffect(() => {
     fetchNews();
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -52,7 +54,7 @@ function App() {
     <>
       {/* Search Function */}
       <header>
-        <div className={styles.search}>
+        <div>
           <input
             placeholder="Search News"
             onChange={(e) => {
@@ -74,14 +76,20 @@ function App() {
       </header>
       {/* Sorting News */}
       <div className={styles.dropdown}>
-        {search !== "" ? (
-          <p>
-            Sorted By: {newcategory.replace(/^./, newcategory[0].toUpperCase())}
-          </p>
-        ) : (
-          <p>Sorted By: {category.replace(/^./, category[0].toUpperCase())}</p>
-        )}
+        <div className={styles.sort}>
+          {search !== "" ? (
+            <p className={styles.sortit}>
+              Sorted By:{" "}
+              {newcategory.replace(/^./, newcategory[0].toUpperCase())}
+            </p>
+          ) : (
+            <p className={styles.sortit}>
+              Sorted By: {category.replace(/^./, category[0].toUpperCase())}
+            </p>
+          )}
+        </div>
         <img
+          className={styles.fiximg}
           src="https://cdn-icons-png.flaticon.com/512/32/32450.png"
           alt=""
           style={{ width: "30px", height: "20px", cursor: "pointer" }}
@@ -91,110 +99,110 @@ function App() {
           }}
         />
       </div>
-      <br />
-      {drop ? (
-        <div className={styles.categories}>
-          {search !== "" ? (
-            <div>
-              <p
-                className={styles.categoriesText}
-                onClick={() => {
-                  SetnewCategory("popularity");
-                  setPage(1);
-                }}
-              >
-                Popularity
-              </p>
-              <p
-                className={styles.categoriesText}
-                onClick={() => {
-                  SetnewCategory("relevancy");
-                  setPage(1);
-                }}
-              >
-                Relevancy
-              </p>
-              <p
-                className={styles.categoriesText}
-                onClick={() => {
-                  SetnewCategory("publishedAt");
-                  setPage(1);
-                }}
-              >
-                Published At
-              </p>
-            </div>
-          ) : (
-            <div>
-              <p
-                className={styles.categoriesText}
-                onClick={() => {
-                  setCategory("general");
-                  setPage(1);
-                }}
-              >
-                General
-              </p>
-              <p
-                className={styles.categoriesText}
-                onClick={() => {
-                  setCategory("business");
-                  setPage(1);
-                }}
-              >
-                Business
-              </p>
-              <p
-                className={styles.categoriesText}
-                onClick={() => {
-                  setCategory("sports");
-                  setPage(1);
-                }}
-              >
-                Sports
-              </p>
-              <p
-                className={styles.categoriesText}
-                onClick={() => {
-                  setCategory("science");
-                  setPage(1);
-                }}
-              >
-                Science
-              </p>
-              <p
-                className={styles.categoriesText}
-                onClick={() => {
-                  setCategory("entertainment");
-                  setPage(1);
-                }}
-              >
-                Entertainment
-              </p>
-              <p
-                className={styles.categoriesText}
-                onClick={() => {
-                  setCategory("health");
-                  setPage(1);
-                }}
-              >
-                Health
-              </p>
-              <p
-                className={styles.categoriesText}
-                onClick={() => {
-                  setCategory("technology");
-                  setPage(1);
-                }}
-              >
-                Technology
-              </p>
-            </div>
-          )}
-        </div>
-      ) : null}
-
       <div>
+        {drop ? (
+          <div className={styles.categories}>
+            {search !== "" ? (
+              <div>
+                <p
+                  className={styles.categoriesText}
+                  onClick={() => {
+                    SetnewCategory("popularity");
+                    setPage(1);
+                  }}
+                >
+                  Popularity
+                </p>
+                <p
+                  className={styles.categoriesText}
+                  onClick={() => {
+                    SetnewCategory("relevancy");
+                    setPage(1);
+                  }}
+                >
+                  Relevancy
+                </p>
+                <p
+                  className={styles.categoriesText}
+                  onClick={() => {
+                    SetnewCategory("publishedAt");
+                    setPage(1);
+                  }}
+                >
+                  Published At
+                </p>
+              </div>
+            ) : (
+              <div>
+                <p
+                  className={styles.categoriesText}
+                  onClick={() => {
+                    setCategory("general");
+                    setPage(1);
+                  }}
+                >
+                  General
+                </p>
+                <p
+                  className={styles.categoriesText}
+                  onClick={() => {
+                    setCategory("business");
+                    setPage(1);
+                  }}
+                >
+                  Business
+                </p>
+                <p
+                  className={styles.categoriesText}
+                  onClick={() => {
+                    setCategory("sports");
+                    setPage(1);
+                  }}
+                >
+                  Sports
+                </p>
+                <p
+                  className={styles.categoriesText}
+                  onClick={() => {
+                    setCategory("science");
+                    setPage(1);
+                  }}
+                >
+                  Science
+                </p>
+                <p
+                  className={styles.categoriesText}
+                  onClick={() => {
+                    setCategory("entertainment");
+                    setPage(1);
+                  }}
+                >
+                  Entertainment
+                </p>
+                <p
+                  className={styles.categoriesText}
+                  onClick={() => {
+                    setCategory("health");
+                    setPage(1);
+                  }}
+                >
+                  Health
+                </p>
+                <p
+                  className={styles.categoriesText}
+                  onClick={() => {
+                    setCategory("technology");
+                    setPage(1);
+                  }}
+                >
+                  Technology
+                </p>
+              </div>
+            )}
+          </div>
+        ) : null}
+      </div>{" "}
+      <div className="cardsssss">
         <div className={styles.container}>
           {news.map((value, index) => {
             return (
@@ -217,20 +225,17 @@ function App() {
           })}
         </div>
       </div>
-
-      <div className="container my-3">
-        <div className="row">
-          <div className="col-4">
-            <button
-              className={styles.button2}
-              onClick={() => {
-                setPage(page + 1);
-              }}
-            >
-              Load More
-            </button>
-          </div>
-        </div>
+      <br />
+      <br />
+      <div className="fetch more news">
+        <button
+          className={styles.button2}
+          onClick={() => {
+            setPage(page + 1);
+          }}
+        >
+          Load More
+        </button>
       </div>
     </>
   );
